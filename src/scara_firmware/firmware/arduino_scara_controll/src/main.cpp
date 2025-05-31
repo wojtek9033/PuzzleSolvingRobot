4117bdb9dab22a86ba9a059a2064a327b81f420e
wojtek9033 <wryn903@gmail.com>
2025-05-31 18:44:13 +0200

    adding wrist to arduino code

--- src/scara_firmware/firmware/arduino_scara_controll/src/main.cpp
+++ src/scara_firmware/firmware/arduino_scara_controll/src/main.cpp
@@ -1,26 +1,4 @@
 #include <Arduino.h>
-/*
-int incomingByte = 0; // for incoming serial data
-int counter = 0;
-    void setup() {
-      Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-    }
-
-    void loop() {
-      // send data only when you receive data:
-      while (Serial.available()) {
-        Serial.println("Received new command!");
-        // read the incoming byte:
-        incomingByte = Serial.read();
-        char c = incomingByte;
-        // say what you got:
-        Serial.print("I received: ");
-        Serial.println(c);
-        Serial.println(counter);
-      }
-      counter++;
-    }
-*/
 
 #include <AccelStepper.h>
 
@@ -32,12 +10,14 @@ int counter = 0;
 #define SHOULDER_DIR_PIN 5
 #define ELBOW_STEP_PIN 6
 #define ELBOW_DIR_PIN 7
-#define WRIST_PIN 0
+#define WRIST_STEP_PIN 8
+#define WRIST_DIR_PIN 9
 
 // clockwise rotation means negative angle
 AccelStepper base(AccelStepper::DRIVER, BASE_STEP_PIN, BASE_DIR_PIN);
 AccelStepper shoulder(AccelStepper::DRIVER, SHOULDER_STEP_PIN, SHOULDER_DIR_PIN);
 AccelStepper elbow(AccelStepper::DRIVER, ELBOW_STEP_PIN, ELBOW_DIR_PIN);
+AccelStepper wrist(AccelStepper::DRIVER, WRIST_STEP_PIN, WRIST_DIR_PIN);
 
 const double MICROSTEPS = 16.0; // Number of microsteps
 const double MOTOR_STEPS_PER_REV = 200.0; // Number of steps per revolution for the motor
@@ -46,22 +26,26 @@ const double STEPS_PER_REV = MOTOR_STEPS * MICROSTEPS; // Total steps per revolu
 const double BASE_SCREW_PITCH = 2.0; // Pitch of the screw in mm  
 const double SHOULDER_GEAR_RATIO = 4.5; // Gear ratio for the shoulder motor
 const double ELBOW_GEAR_RATIO = 7.28; // Gear ratio for the elbow motor
+const double WRIST_GEAR_RATIO = 3.0;
 
 // Status of axis
 bool baseIsMoving = false;
 bool shoulderIsMoving = false;
 bool elbowIsMoving = false;
+bool wristwIsMoving = false;
 
 // For slope detection
 bool prev_baseIsMoving = false;
 bool prev_shoulderIsMoving = false;
 bool prev_elbowIsMoving = false;
+bool prev_wristIsMoving = false;
 
 // Buffer for receiving a command
 String serialData = "";
 String baseStatus = "";
 String shoulderStatus = "";
 String elbowStatus = "";
+String wristStatus = "";
 String status = "";
 
 void moveAxisRevolute(AccelStepper& motor, double angle, double gear_ratio);
@@ -73,15 +57,17 @@ void setup() {
   base.setCurrentPosition(0); 
   shoulder.setCurrentPosition(0);
   elbow.setCurrentPosition(0);
+  wrist.setCurrentPosition(0);
 
   base.setMaxSpeed(STEPS_PER_REV * 6.0);
   shoulder.setMaxSpeed(STEPS_PER_REV  * 2.0);
   elbow.setMaxSpeed(STEPS_PER_REV * 2.0);
+  wrist.setMaxSpeed(STEPS_PER_REV);
 
   base.setAcceleration(STEPS_PER_REV * 4.0);
   shoulder.setAcceleration(STEPS_PER_REV * 4.0);
   elbow.setAcceleration(STEPS_PER_REV * 4.0);
-  
+  wrist.setAcceleration(STEPS_PER_REV);
 
   Serial.begin(9600);
 }
@@ -90,6 +76,7 @@ void loop() {
   prev_baseIsMoving = baseIsMoving;
   prev_shoulderIsMoving = shoulderIsMoving;
   prev_elbowIsMoving = elbowIsMoving;
+  prev_wristIsMoving = wristwIsMoving;
 
   if (Serial.available()) {
     char chr = Serial.read();
@@ -109,8 +96,9 @@ void loop() {
   shoulderIsMoving = shoulder.run();
   elbowIsMoving = elbow.run();
 
-  if ((prev_baseIsMoving || prev_elbowIsMoving || prev_shoulderIsMoving) && (!baseIsMoving && !shoulderIsMoving && !elbowIsMoving)) {
-      Serial.println("1,1,1"); //ASSUMING STEPS ARE NEVER LOST - WE ONLY SEND DATA WHEN MOVEMENT IS DONE
+  if ((prev_baseIsMoving || prev_elbowIsMoving || prev_shoulderIsMoving || prev_wristIsMoving) 
+      && (!baseIsMoving && !shoulderIsMoving && !elbowIsMoving && !wristwIsMoving)) {
+      Serial.println("1,1,1,1"); //ASSUMING STEPS ARE NEVER LOST - WE ONLY SEND DATA WHEN ALL MOVEMENT IS DONE
   }
 }
 
@@ -140,6 +128,11 @@ void parseCommand(String cmd) {
       Serial.print(position);
       Serial.print("\n");
       moveAxisRevolute(elbow, position, ELBOW_GEAR_RATIO);
+    } else if (axis == 'w') {
+      Serial.print("Axis wrist, position: ");
+      Serial.print(position);
+      Serial.print("\n");
+      moveAxisRevolute(wrist, position, WRIST_GEAR_RATIO);
     }
     lastIdx = nextIdx + 1;
   }
